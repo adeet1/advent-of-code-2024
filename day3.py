df618bf36179e694192edbf5915adf96b1c95416
adeet1 <adeet.patel@gmail.com>
2024-12-03 16:47:55 -0500

    Day 3, part 2

--- day3.py
+++ day3.py
@@ -18,3 +18,26 @@ for mults in mults_list:
         result += int(a) * int(b)
 
 print("Part 1:", result)
+
+regex = "(?:do\(\))|(?:don't\(\))|mul\(\d+,\d+\)"
+
+mults_list = list(map(lambda line: re.findall(regex, line), lines))
+
+result = 0
+multEnabled = True
+for mults in mults_list:
+    # For each instruction, extract the two numbers and multiply them
+    for mult in mults:
+        if mult == "do()":
+            multEnabled = True
+            continue
+        elif mult == "don't()":
+            multEnabled = False
+            continue
+
+        if multEnabled:
+            numbers = mult[:-1].split("(")[-1]
+            a, b = numbers.split(",")
+            result += int(a) * int(b)
+
+print("Part 2:", result)
