56456e43788441ebe9c02a9d5da863ab933be08d
Adeet Patel <adeet.patel@gmail.com>
2024-12-10 21:10:07 -0500

    Day 7 (#5)

---
+++ day7.py
@@ -0,0 +1,54 @@
+import itertools
+import re
+
+puzzle = []
+with open("input/day7.txt", "r") as file:
+    for line in file:
+        tmp = line.split(":")
+        values = tmp[1].split(" ")
+        newRow = [tmp[0]] + values[1:]
+        puzzle.append([int(x) for x in newRow])
+
+def concat_operator(a, b):
+    if a == 0:
+        return b
+
+    return int(str(a) + str(b))
+
+def executeRow(testValue, operands):
+    # print("Test value", testValue, "with operands", operands)
+    operatorCombos = list(itertools.product(["+", "*", "||"], repeat=len(operands)-1))
+    
+    for operatorCombo in operatorCombos:
+        expression = str(operands[0])
+        print("Evaluating starting expression", expression)
+
+        # Form the full expression
+        for i in range(len(operatorCombo)):
+            # forgot to put str(eval) and just did eval which is why i got int str concat errors
+            tmp = eval("(" + expression + ")", {"concat_operator": concat_operator})
+
+            expression = str(tmp) + operatorCombo[i] + str(operands[i+1])
+
+            if operatorCombo[i] == "||":
+                expression = re.sub(r"(\d+)\s*\|\|\s*(\d+)", r"concat_operator(\1, \2)", expression)
+
+            # print("Evaluating expression:", testValue, "?=", expression)
+        
+        # Evaluate the full expression
+        if testValue == eval(expression, {"concat_operator": concat_operator}):
+            print("----", testValue, "VALIDATES ----")
+            return True
+    
+    print("----", testValue, "invalid ----")
+    return False
+
+totalCalibrationResult = 0
+for row in puzzle:
+    testValue = row[0]
+    operands = row[1:]
+
+    if executeRow(testValue, operands):
+        totalCalibrationResult += testValue
+
+print("Part 2:", totalCalibrationResult)
