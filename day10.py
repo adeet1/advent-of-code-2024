86a666d7b0fa69810763a63a9b77d2bfbf4da828
adeet1 <adeet.patel@gmail.com>
2024-12-12 23:25:19 -0500

    Only need to count when we hit a 9, but the code doesn't seem to work

--- day10.py
+++ day10.py
@@ -34,39 +34,38 @@ def inBounds(r, c):
         return False
     return True
 
-# run DFS at each trailhead
-def DFS(r, c, trailSoFar):
-    print("DFS({}, {}, {})".format(r, c, trailSoFar))
-    
-    visited[(r, c)] = True
+# Count the number of 9s visited from each trailhead
+def computeTrailheadScore(r, c, currentScore=0):
+    print("computeTrailheadScore({}, {}, {})".format(r, c, currentScore))
 
     height = topMap[r][c]
     # base case: we found a complete hiking trail
     if height == 9:
-        return trailSoFar
+        currentScore += 1
+        return currentScore
 
     # look up, down, left, right
     if inBounds(r-1, c):
         print("up from", (r, c))
-        if not visited[(r-1, c)] and topMap[r-1][c] == 1 + height:
-            return DFS(r-1, c, trailSoFar + [(r-1, c)])
+        if topMap[r-1][c] == 1 + height:
+            return computeTrailheadScore(r-1, c, currentScore)
 
     if inBounds(r+1, c):
         print("down from", (r, c))
-        if not visited[(r+1, c)] and topMap[r+1][c] == 1 + height:
-            return DFS(r+1, c, trailSoFar + [(r+1, c)])
+        if topMap[r+1][c] == 1 + height:
+            return computeTrailheadScore(r+1, c, currentScore)
     
     if inBounds(r, c-1):
         print("left from", (r, c))
-        if not visited[(r, c-1)] and topMap[r][c-1] == 1 + height:
-            return DFS(r, c-1, trailSoFar + [(r, c-1)])
+        if topMap[r][c-1] == 1 + height:
+            return computeTrailheadScore(r, c-1, currentScore)
     
     if inBounds(r, c+1):
         print("right from", (r, c))
-        if not visited[(r, c+1)] and topMap[r][c+1] == 1 + height:
-            return DFS(r, c+1, trailSoFar + [(r, c+1)])
+        if topMap[r][c+1] == 1 + height:
+            return computeTrailheadScore(r, c+1, currentScore)
 
-    return trailSoFar
+    return currentScore
 
 # every time we reach a 9, increment the score for the trailhead we started from
 trails = {}
@@ -76,12 +75,11 @@ for trailhead in trailheadScores.keys():
     # Array to keep track of which locations are visited
     visited = defaultdict(bool)
 
-    trails[trailhead] = DFS(r, c, [(r, c)])
-    
+    trails[trailhead] = computeTrailheadScore(r, c)
+print("")
 print(trails)
 
 """
-1 2 1
-0 1 2
-1 0 2
+{(0, 2): [(0, 2), (1, 2), (1, 3), (2, 3), (3, 3), (3, 2), (4, 2), (4, 3), (4, 4), (3, 4)], (0, 4): [(0, 4), (1, 4), (1, 3), (2, 3), (3, 3), (3, 2), (4, 2), (4, 3), (4, 4), (3, 4)], (2, 4): [(2, 4), (1, 4), (1, 3), (2, 3), (3, 3), (3, 2), (4, 2), (4, 3), (4, 4), (3, 4)], (4, 6): [(4, 6), (5, 6), (5, 7), (4, 7), (3, 7), (2, 7), (2, 6), (1, 6), (1, 5), (2, 5)], (5, 2): [(5, 2), (5, 3), (6, 3), (6, 2), (7, 2), (7, 3), (7, 4), (7, 5), (6, 5), (6, 4)], (5, 5): [(5, 5), (5, 6), (5, 7), (4, 7), (3, 7), (2, 7), (2, 6), (1, 6), (1, 5), (2, 5)], (6, 0): [(6, 0), (7, 0)], (6, 6): [(6, 6), (5, 6), (5, 7), (4, 7), (3, 7), (2, 7), (2, 6), (1, 6), (1, 5), (2, 5)], (7, 1): [(7, 1), (6, 1), (5, 1), (5, 0), (4, 0), (4, 1), (3, 1), (2, 1), (1, 1), (0, 1)]}
+
 """
