b884b0f7bfa641f44ff8bc624d4aa7fdcfc67f24
adeet1 <adeet.patel@gmail.com>
2024-12-13 00:21:04 -0500

    Day 10, part 2

--- day10.py
+++ day10.py
@@ -11,7 +11,6 @@ with open("input/day10.txt", "r") as file:
         topMap.append([int(x) for x in row])
 
 topMap = np.array(topMap)
-print(topMap)
 
 # Checks that a set of coordinates is inside the grid
 def inBounds(r, c):
@@ -23,9 +22,8 @@ def inBounds(r, c):
 
 # Count the number of 9s visited from each trailhead
 def computeTrailheadScore(r, c, nines=set()):
-    print("computeTrailheadScore({}, {})".format(r, c))
-
     height = topMap[r][c]
+
     # base case: we found a stop
     if height == 9:
         nines.add((r, c))
@@ -33,22 +31,18 @@ def computeTrailheadScore(r, c, nines=set()):
 
     # look up, down, left, right
     if inBounds(r-1, c):
-        print("up from", (r, c))
         if topMap[r-1][c] == 1 + height:
             computeTrailheadScore(r-1, c, nines)
 
     if inBounds(r+1, c):
-        print("down from", (r, c))
         if topMap[r+1][c] == 1 + height:
             computeTrailheadScore(r+1, c, nines)
     
     if inBounds(r, c-1):
-        print("left from", (r, c))
         if topMap[r][c-1] == 1 + height:
             computeTrailheadScore(r, c-1, nines)
     
     if inBounds(r, c+1):
-        print("right from", (r, c))
         if topMap[r][c+1] == 1 + height:
             computeTrailheadScore(r, c+1, nines)
 
@@ -57,8 +51,6 @@ for r in range(len(topMap)):
     for c in range(len(topMap[r])):
         if topMap[r][c] == 0:
             trailheadScores[(r, c)] = 0
-
-print(trailheadScores.keys())
 
 trails = {}
 for trailhead in trailheadScores.keys():
@@ -69,3 +61,49 @@ for trailhead in trailheadScores.keys():
     trails[trailhead] = len(stops)
 
 print("Part 1:", sum(trails.values()))
+
+"""
+The number of trails that begin at (r, c) is the sum
+of the number of trails that begin from the top, bottom,
+left, and right of that location.
+
+Ex: there are 4 trails beginning from the 8.
+  9
+9 8 9
+  9
+"""
+def computeTrailRating(r, c):
+    height = topMap[r][c]
+    if height == 9:
+        return 1
+
+    score = 0
+
+    # up
+    if inBounds(r-1, c):
+        if topMap[r-1][c] == 1 + height:
+            score += computeTrailRating(r-1, c)
+    
+    # down
+    if inBounds(r+1, c):
+        if topMap[r+1][c] == 1 + height:
+            score += computeTrailRating(r+1, c)
+    
+    # left
+    if inBounds(r, c-1):
+        if topMap[r][c-1] == 1 + height:
+            score += computeTrailRating(r, c-1)
+    
+    # right
+    if inBounds(r, c+1):
+        if topMap[r][c+1] == 1 + height:
+            score += computeTrailRating(r, c+1)
+    
+    return score
+
+trailRatings = {}
+for trailhead in trailheadScores.keys():
+    r, c = trailhead
+    trailRatings[trailhead] = computeTrailRating(r, c)
+
+print("Part 2:", sum(trailRatings.values()))
